2854b31ea284d6fc441ee4a6b6b87240df775741
LuisCaBar <90533685+LuisCaBar@users.noreply.github.com>
2022-01-31 20:10:50 -0500

    Update About.js
    main

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,6 +7,7 @@ const About = () => {
         <div className="row about-content">
             <div className="col-7 offset-3">
             <Accordion defaultActiveKey="0">
+
   <Accordion.Item eventKey="0">
     <Accordion.Header>Accordion Item #1</Accordion.Header>
     <Accordion.Body>
@@ -19,6 +20,7 @@ const About = () => {
       est laborum.
     </Accordion.Body>
   </Accordion.Item>
+
   <Accordion.Item eventKey="1">
     <Accordion.Header>Accordion Item #2</Accordion.Header>
     <Accordion.Body>
@@ -31,7 +33,24 @@ const About = () => {
       est laborum.
     </Accordion.Body>
   </Accordion.Item>
+
+
+<Accordion.Item eventKey="2">
+    <Accordion.Header>Accordion Item #3</Accordion.Header>
+    <Accordion.Body>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </Accordion.Body>
+  </Accordion.Item>
 </Accordion>
+
+
+
             </div>
         </div>
     </section>
